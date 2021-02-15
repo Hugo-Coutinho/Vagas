48789ad1dc35c140db7f4897069fd62d0abd9d9b
Hugo Coutinho <adm@adms-MacBook-Pro.local>
2021-02-15 16:19:58 -0300

    first commit

---
+++ Vagas/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Vagas
+//
+//  Created by Hugo Coutinho on 15/02/21.
+//  Copyright © 2021 Hugo Coutinho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
